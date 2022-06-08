3a37ef75098b332ad87d6e2d64641c1399529880
zaflaton <59338890+zaflaton@users.noreply.github.com>
2022-06-08 23:12:38 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-shopify-ecommerce`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-shopify',
+    options: {
+      "shopName": "countrywatch.myshopify.com",
+      "accessToken": "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"
+    }
+  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
